0fdeb19f9411718a17573679afa31f9055c679f0
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-14 23:56:23 +0600

    stores js :tada:

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import reloadSlice from './Slice/reloadSlice';
 import toolsSlice from './Slice/toolsSlice';
 import searchSlice from './Slice/searchSlice';
 
-
+// Store JS
 export const store = configureStore({
     reducer: {
         user: userSlice, // Add your slice reducer to the store
@@ -14,4 +14,4 @@ export const store = configureStore({
     },
 });
 
-// Store JS
+
